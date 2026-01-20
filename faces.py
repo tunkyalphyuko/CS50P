20f83dc0be9e126dbab45f71d4579f753e6fecb8
Tun Kyal Phyu Ko <tunkyalphyuko49@gmail.com>
2026-01-20 17:02:35 +0630

    Delete faces.py

--- faces.py
+++
@@ -1,9 +0,0 @@
-def main():
-    user_input = input("Enter an emoticon! ")
-    result = convert(user_input)
-    print(result)
-
-def convert(text):
-    return text.replace(":)", "🙂").replace(":(", "🙁")
-
-main()
