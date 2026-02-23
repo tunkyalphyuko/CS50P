5ddf9e1f9ac38b3979bd7d5d085bb262e283cb1b
Tun Kyal Phyu Ko <tunkyalphyuko49@gmail.com>
2026-02-24 00:08:46 +0630

    Refactor music handling and story recommendation

--- week2/psets2/project2.py
+++ week2/psets2/project2.py
@@ -1,12 +1,7 @@
 import webbrowser
 from pygame import mixer
 
-music = "piano.mp3"
-
-def main():
-    mixer.init()
-
-    stories = {
+stories = {
         "feminism": {
             "name": "The Story of an Hour",
             "link": "https://americanliterature.com/author/kate-chopin/short-story/the-story-of-an-hour"
@@ -21,27 +16,31 @@ def main():
         }
     }
 
+def bg_music(music):
+    try:
+        mixer.init()
+        mixer.music.load(music)
+        mixer.music.play(-1)
+    except Exception:
+        pass
+
+def main():
     genre = input("What kind of story do you want? (Feminism, Horror, Classic): ").lower()
 
     if genre in stories:
         story = stories[genre]
-
-        try:
-            mixer.music.load(music)
-            mixer.music.play(-1)
-        except:
-            print("")
 
         print(f"I recommend {story['name']}.")
         print(f"Read it here: {story['link']}")
 
         webbrowser.open(story['link'])
 
+        bg_music("piano.mp3")
+
         input("Enter to stop the music and exit.")
         mixer.music.stop()
         
     else:
         print("Sorry I don't have a recommendation for that genre yet.")
-
 
 main()
